7dffdc97e901edf64d9df0c9946140060bb3608c
naeem <naeemislamjr182@gmail.com>
2023-10-13 03:54:12 +0600

    load data and show

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Users from './components/Users.jsx';
 
 
 
@@ -13,6 +14,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+  },
+  {
+    path: "/users",
+    element: <Users></Users>,
+    loader: () => fetch('http://localhost:5000/users')
   },
 ]);
 
