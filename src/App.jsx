7879f44539e0d6d461d9fc2189f0130899832fa3
naeem <naeemislamjr182@gmail.com>
2023-10-13 02:35:27 +0600

    form create and take value

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import './App.css'
 
 function App() {
 
+  const handleAddUser = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    console.log(user)
+
+  }
 
   return (
     <>
 
       <h1>Simpe crud</h1>
-
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
