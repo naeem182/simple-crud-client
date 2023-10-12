d1f93a4e3159e0c8b0cc87b9c6e7dfc7e3a57efe
naeem <naeemislamjr182@gmail.com>
2023-10-13 05:34:08 +0600

    take dynamic user  before update

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 
 const Users = () => {
     const loadedUsers = useLoaderData()
@@ -40,9 +40,13 @@ const Users = () => {
             <div>
                 {
                     users.map(user => <p key={user._id}
-                    >{user.name} : {user.email}: <button
-                        onClick={() => handleDelete(user._id)}
-                    >X</button> </p>)
+                    >{user.name} : {user.email}:
+                        <Link to={`/update/${user._id}`}>
+                            <button>Update</button>
+                        </Link>
+                        <button
+                            onClick={() => handleDelete(user._id)}
+                        >X</button> </p>)
                 }
             </div>
         </div>
